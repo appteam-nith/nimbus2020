78928d55fd522ecd1d3b364597975708b3dd4d67
pawanharariya <pawanharariya@gmail.com>
2020-01-31 22:49:32 +0530

    login completed

--- app/src/main/java/com/nith/appteam/nimbus2020/Activities/MainActivity.java
+++ app/src/main/java/com/nith/appteam/nimbus2020/Activities/MainActivity.java
@@ -1,20 +1,34 @@
 package com.nith.appteam.nimbus2020.Activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import com.nith.appteam.nimbus2020.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.nith.appteam.nimbus2020.R;
+
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent intent=new Intent(this,QuizMainActivity.class);
+        sharedPref = getSharedPreferences("app", MODE_PRIVATE);
+        editor = sharedPref.edit();
+        //Checking whether user has logged in or not
+        if (!sharedPref.getBoolean("loginStatus", false)) {
+            Intent i = new Intent(this, Login.class);
+            startActivity(i);
+            finish();
+        }
+        //Checking whether user has created profile or not
+//        if(sharedPref.getBoolean("profileCreated",false)){
+//
+//        }
+        Intent intent = new Intent(this, QuizMainActivity.class);
         startActivity(intent);
     }
 }
