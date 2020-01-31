5d24ff6ab8f6736ea07b938cdd74d6bed4e77a1e
Monu Kumar <monuk7735@gmail.com>
2020-01-31 17:59:01 +0530

    Update 1.1

---
+++ app/src/main/java/com/nith/appteam/nimbus2020/Activities/Schedule.java
@@ -0,0 +1,16 @@
+package com.nith.appteam.nimbus2020.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nith.appteam.nimbus2020.R;
+
+public class Schedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
