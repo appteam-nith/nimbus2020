b077475d63bbf325712811ce2a532a46c1017447
pawanharariya <pawanharariya@gmail.com>
2020-02-01 23:01:33 +0530

    Campus Ambassador post done

--- app/src/main/java/com/nith/appteam/nimbus2020/Activities/MainActivity.java
+++ app/src/main/java/com/nith/appteam/nimbus2020/Activities/MainActivity.java
@@ -8,12 +8,8 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.cloudinary.android.MediaManager;
 import com.nith.appteam.nimbus2020.R;
 import com.squareup.picasso.Picasso;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -27,32 +23,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Required for cloudinary
-        Map config = new HashMap();
-        config.put("cloud_name", "duutozrvz");
-        MediaManager.init(MainActivity.this, config);
+
         profileButton = findViewById(R.id.profile_button);
         post = findViewById(R.id.post);
         sharedPref = getSharedPreferences("app", MODE_PRIVATE);
         editor = sharedPref.edit();
+
         //Checking whether user has logged in or not
-//        if (!sharedPref.getBoolean("loginStatus", false)) {
-//            Intent i = new Intent(this, Login.class);
-//            startActivity(i);
-//            finish();
-//        }
+        if (sharedPref.getBoolean("loginStatus", false) == false) {
+            Intent i = new Intent(this, Login.class);
+            startActivity(i);
+            finish();
+        }
 
         //Checking whether user has created profile or not
-//        if (!sharedPref.getBoolean("profileStatus", false)) {
-//            Intent i = new Intent(this, ProfileNew.class);
-//            startActivity(i);
-//            finish();
-//        }
+        if (sharedPref.getBoolean("profileStatus", false) == false) {
+            Intent i = new Intent(this, ProfileNew.class);
+            startActivity(i);
+            finish();
+        }
 
         Picasso.with(MainActivity.this)
                 .load(sharedPref.getString("imageUrl", String.valueOf(R.string.defaultImageUrl)))
-                .resize(80, 80)
-                .centerCrop()
+
                 .into(profileButton);
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
